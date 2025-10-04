23b7a2350be0cfedaa1a197b2a47ebaf63f1970b
Trung Nguyen <trung.nguyenduc.dev@gmail.com>
2025-10-04 11:28:51 +0700

    Fix: change swagger path from /api to /docs

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,37 +1,39 @@
-import {NestFactory} from '@nestjs/core';
-import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
-import {AppModule} from './app.module';
-import {ValidationPipe} from "@nestjs/common";
-import {initializeTransactionalContext} from 'typeorm-transactional';
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { initializeTransactionalContext } from 'typeorm-transactional';
 
 async function bootstrap() {
-    initializeTransactionalContext();
+  initializeTransactionalContext();
 
-    const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(new ValidationPipe({
-        whitelist: true,
-        transform: true, // IMPORTANT: turn on type auto-transformation
-        transformOptions: {
-            enableImplicitConversion: true,
-        },
-    }));
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true, // IMPORTANT: turn on type auto-transformation
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
 
-    const config = new DocumentBuilder()
-        .setTitle('F8K13')
-        .setDescription('The API description')
-        .setVersion('1.0')
-        .addTag('LMS')
-        .addBearerAuth()
-        .build();
-    const documentFactory = () => SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, documentFactory);
+  const config = new DocumentBuilder()
+    .setTitle('F8K13')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('LMS')
+    .addBearerAuth()
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
 
-    app.enableCors({
-        origin: process.env.FRONTEND_URL, // allow front-end access
-        credentials: true, // use authentication
-    });
+  app.enableCors({
+    origin: process.env.FRONTEND_URL, // allow front-end access
+    credentials: true, // use authentication
+  });
 
-    await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 
 bootstrap();
